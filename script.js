ade822be033364f2629c0b16822339bae9ddfb1d
izza iqbal <izza240803@gmail.com>
2020-10-15 19:35:36 +0700

    project tim b06

---
+++ script.js
@@ -0,0 +1,14 @@
+$(`.page-scroll`).on(`click`, function(e) {
+
+    // ambil isi href
+    var tujuan = $(this).attr(`href`);
+    var elementTujuan = $(tujuan);
+
+    // pindah scroll
+    $(`html, body`).animate({
+        scrolltop: elementTujuan.offset().top - 50
+    },1250, 'swing');
+
+    e.preventDefault();
+
+});
